6b0a5f10b63652f6c42a25232cfc6a503eae0e99
drew <sxswfuture@gmail.com>
2018-05-31 21:23:22 -0400

    Initial Commit

---
+++ festifind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  festifind
+//
+//  Created by Andrew Gonzales-Raines on 5/31/18.
+//  Copyright © 2018 Andrew Gonzales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
